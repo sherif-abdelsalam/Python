0821b8c6d29570c4be43c503a00244183a0017fe
Sherif AbdleSalam <sherif.abdelsalam.111@gmail.com>
2025-05-01 13:46:10 +0300

    ehanced leap year

--- is_leap.py
+++ is_leap.py
@@ -1,6 +1,5 @@
 def is_leap(year):
-    leap = year % 400 == 0 or (year % 4 ==0 and year%100 !=0)    
-    return leap
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 year = int(input())
 print(is_leap(year))
